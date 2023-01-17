dcffbd27ded46ef939fb87f731e6972189ace40c
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-16 21:13:26 -0600

    Reusable Widgets

--- lib/home.dart
+++ lib/home.dart
@@ -22,16 +22,92 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.amber,
               ),
               Container(
-                height: size.height / 1.7,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 92, 95, 102),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
+                  height: size.height / 1.7,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 92, 95, 102),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    ),
                   ),
-                ),
-              )
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          blocks("AC", Colors.pink),
+                          blocks("CC", Colors.pink),
+                          blocks("%", Colors.pink),
+                          blocks("/", Colors.amberAccent)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          blocks("1"),
+                          blocks("2"),
+                          blocks("3"),
+                          blocks("X", Colors.amberAccent)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          blocks("4"),
+                          blocks("5"),
+                          blocks("6"),
+                          blocks("+", Colors.amberAccent)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          blocks("7"),
+                          blocks("8"),
+                          blocks("9"),
+                          blocks("-", Colors.amberAccent)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          blocks("1"),
+                          blocks("2"),
+                          blocks("3"),
+                          blocks("=", Colors.amberAccent)
+                        ],
+                      ),
+                    ],
+                  ))
             ],
+          )),
+    );
+  }
+
+// Repeated button blocks
+  Widget blocks(String buttonText, [Color? buttonColor]) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 14, left: 8, right: 8),
+      child: Container(
+          height: 70,
+          width: 70,
+          decoration: const BoxDecoration(
+            color: Color(
+              0xff22252D,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                  //  // make default color (if not specified) by using ?? nullable color
+                  color: buttonColor ?? Colors.blue,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
           )),
     );
   }
