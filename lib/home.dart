1aea71f7cb5435d73e9d087e6d3b96961a52bfd8
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-16 20:49:16 -0600

    Basic UI

--- lib/home.dart
+++ lib/home.dart
@@ -10,8 +10,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 50, 0, 251),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: size.height / 15,
+                color: Colors.amber,
+              ),
+              Container(
+                height: size.height / 1.7,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 92, 95, 102),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
